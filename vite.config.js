267fcfed99f66700749e952cbb512e8a993358f6
Natty <natty@Nattys-MacBook-Air.local>
2025-05-18 19:39:42 +0700

    Fix vite entry point and config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',            // make sure root is current directory
+  publicDir: 'public',  // optional if you're using public/
+  build: {
+    outDir: 'dist'
+  }
 })
