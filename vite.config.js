48e4d61b646787572a25af9eb9da215163d1ff24
Natty <natty@Nattys-MacBook-Air.local>
2025-05-18 19:54:18 +0700

    Fix: remove custom root and publicDir for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',            // make sure root is current directory
-  publicDir: 'public',  // optional if you're using public/
   build: {
     outDir: 'dist'
   }
